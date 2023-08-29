2de4d7b09968029b0c82f481f711992097ae35f0
Subhadeep Hazra <124555668+subhadeephazra@users.noreply.github.com>
2023-08-29 11:42:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function createRandomPromise() {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+          resolve("Hello, world!");
+		},1000);
+	});
+}
+createRandomPromise()
+	.then((result) => {
+		const div = document.getElementById("output");
+		div.textContent = result;
+	})
+	.catch((error) => {
+		console.error("All promises rejected:", error);
+	});
